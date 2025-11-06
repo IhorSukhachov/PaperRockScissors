613c607782c870cbb268d2208b7c6536cd8454af
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-06 10:52:59 +0200

    created necessary variables

--- PaperRockScissors/ContentView.swift
+++ PaperRockScissors/ContentView.swift
@@ -6,8 +6,20 @@
 //
 
 import SwiftUI
+let possibleMoves: [String] = ["rock", "paper", "scissors"]
+
+
+
 
 struct ContentView: View {
+    @State private var appMove: Int = Int.random(in: 0...2)
+    @State private var userMove: Int = 0
+    @State private var shouldWin = true
+    @State private var score = 0
+    @State private var round = 1
+    
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
