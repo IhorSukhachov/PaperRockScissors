016a3950f7eebb286c443728f9dd44377b1b4de3
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-06 10:38:20 +0200

    Initial Commit

---
+++ PaperRockScissors/PaperRockScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperRockScissorsApp.swift
+//  PaperRockScissors
+//
+//  Created by Ihor Sukhachov on 06.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PaperRockScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
